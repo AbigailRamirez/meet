93546f2b38a0eb4dcb34b9ba2e12f482f7d3af0a
AbigailRamirez <ramirezabigail01@gmail.com>
2023-05-12 15:12:03 -0400

    added puppeteer package

--- src/__tests__/Event.test.js
+++ src/__tests__/Event.test.js
@@ -15,13 +15,13 @@ describe("<Event /> component", () => {
       display.simulate("click");
       const summary = EventWrapper.find(".title");
       const details = EventWrapper.find(".details li");
-      expect(summary.text()).toBe(`Summary: ${event.summary}`);
+      expect(summary.text()).toBe(`Title: ${event.summary}`);
       expect(details.at(0).text()).toBe(`Description: ${event.description}`);
-      expect(details.at(1).text()).toBe(`Location: ${event.location}`);
-      expect(details.at(2).text()).toBe(
+      //expect(details.at(1).text()).toBe(`Location: ${event.location}`);
+      expect(details.at(1).text()).toBe(
         `Start: ${new Date(event.start.dateTime).toISOString()}`
       );
-      expect(details.at(3).text()).toBe(
+      expect(details.at(2).text()).toBe(
         `End: ${new Date(event.end.dateTime).toISOString()}`
       );
     });
