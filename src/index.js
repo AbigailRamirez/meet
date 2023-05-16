a29da423217ff615e66c14ceb6e514cc5e2e78cc
AbigailRamirez <ramirezabigail01@gmail.com>
2023-05-16 17:22:50 -0400

    atatus implementation

--- src/index.js
+++ src/index.js
@@ -6,6 +6,9 @@ import App from './App';
 //import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
+import * as atatus from 'atatus-spa';
+atatus.config('714b79b54b0a49dc81485bfe9b80029e').install();
+
 /*const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
@@ -25,3 +28,4 @@ render(<App />, root);
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
