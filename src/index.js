72aa68368eb9385dc5b9356bff7adf5c1d69cb9c
AbigailRamirez <ramirezabigail01@gmail.com>
2023-05-25 12:02:09 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { render } from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './service-worker';
+//import * as serviceWorker from './service-worker';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
@@ -24,8 +24,10 @@ render(<App />, root);
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration();
-serviceWorker.register();
+//serviceWorker.register();
+//serviceWorker.register();
+serviceWorkerRegistration.register();
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
